536ded2f507563265ae940d89d6ac11655e2ec04
Atma1 <atmasusanto08@gmail.com>
2024-09-03 11:16:44 +0800

    add schema validation for favorite insert

--- src/favorites/favorites.controller.js
+++ src/favorites/favorites.controller.js
@@ -4,6 +4,9 @@ const {
   deleteFavorite,
   insertUserFavorite,
 } = require("./favorites.service");
+const favoriteInsertSchema = require("../schema/favorite.ts");
+const { fromError } = require('zod-validation-error');
+const { ZodError } = require("zod");
 const router = Router();
 
 router.get("/users/:user_id/follows", async (req, res) => {
@@ -18,12 +21,16 @@ router.get("/users/:user_id/follows", async (req, res) => {
 
 router.post("/favorites/add", async (req, res) => {
   try {
+    favoriteInsertSchema.parse(req.body);
     await insertUserFavorite(req.body);
     res.send({
       message: "Success",
     });
   } catch (error) {
-    res.status(400).send(error.message);
+    if (error instanceof ZodError) {
+      return res.status(400).send({ message: fromError(error).toString() });
+    }
+    res.status(500).send(error.message);
   }
 });
 
