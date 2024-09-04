bd955337bc73fab529e362d83ec0bb4fba622e44
Atma1 <atmasusanto08@gmail.com>
2024-09-04 12:28:25 +0800

    add schema documentation

--- src/schema/event.js
+++ src/schema/event.js
@@ -1,5 +1,21 @@
 const z = require("zod");
 
+/**
+ * Schema for inserting an event.
+ * @typedef {Object} eventInsertSchema
+ * @property {string} channel_id - The ID of the channel.
+ * @property {string} category_id - The ID of the category.
+ * @property {string} tag_id - The ID of the tag.
+ * @property {string} name - The name of the event.
+ * @property {string} description - The description of the event.
+ * @property {string} image - The URL of the event image.
+ * @property {string} location - The location of the event.
+ * @property {boolean} is_paid - Indicates if the event is paid or not.
+ * @property {number} price - The price of the event.
+ * @property {string} status - The status of the event. Can be "PENDING", "ONGOING", or "DONE".
+ * @property {string} link_group - The link group of the event.
+ * @property {Date} event_date - The date of the event.
+ */
 const eventInsertSchema = z.object({
     channel_id: z.string(),
     category_id: z.string(),
--- src/schema/favorite.js
+++ src/schema/favorite.js
@@ -1,5 +1,11 @@
 const z = require("zod");
 
+/**
+ * favoriteInsertSchema represents the schema for inserting a favorite.
+ * @typedef {Object} favoriteInsertSchema
+ * @property {string} user_id - The ID of the user. Required.
+ * @property {string} event_id - The ID of the event. Required.
+ */
 const favoriteInsertSchema = z.object({
     user_id: z.string({
         required_error: "user_id is required!"
